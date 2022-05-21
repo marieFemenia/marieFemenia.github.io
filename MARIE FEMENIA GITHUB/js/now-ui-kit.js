84b5bd136d9d492bbae4d2c5e47aa3e92fe3355f
marieFemenia <38974400+marieFemenia@users.noreply.github.com>
2022-05-21 20:03:40 +0200

    Add files via upload

--- MARIE FEMENIA GITHUB/js/now-ui-kit.js
+++ MARIE FEMENIA GITHUB/js/now-ui-kit.js
@@ -235,3 +235,23 @@ function debounce(func, wait, immediate) {
         if (immediate && !timeout) func.apply(context, args);
     };
 };
+
+let list = document.querySelectorAll('.list');
+let itemBox = document.querySelectorAll('.itemBox');
+for(let i = 0; i<list.length; i++){
+    list[i].addEventListener('click', function(){
+        for(let j = 0; j<list.length; j++){
+            list[j].classList.remove('active');
+        }
+        this.classList.add('active');
+        let dataFilter = this.getAttribute('data-filter');
+        for(let k = 0; k<itemBox.length; k++){
+            itemBox[k].classList.remove('active');
+            itemBox[k].classList.add('hide');
+        
+        if(itemBox[k].getAttribute('data-item') == dataFilter || dataFilter == "all"){
+            itemBox[k].classList.remove('hide');
+            itemBox[k].classList.add('active');
+        }
+    }
+    })
